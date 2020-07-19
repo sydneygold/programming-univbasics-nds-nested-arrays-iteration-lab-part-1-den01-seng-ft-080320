79ba8cbc1967a1a49b444459bee121ba10bb4d8e
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-19 19:26:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def find_even_values(src)
   row_index = 0
   element_index = 0
   while row_index < src.count do
-    if src[row_index][element_index] .even?
+    if src[row_index][element_index].even?
       p src[row_index][element_index]
         row_index += 1
     end
